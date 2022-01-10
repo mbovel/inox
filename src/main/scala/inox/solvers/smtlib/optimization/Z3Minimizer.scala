6366374fcab0e5258746c97a42327caeac883232
Solal Pirelli <solal.pirelli@gmail.com>
2022-01-10 17:01:22 +0100

    Fix SO

--- src/main/scala/inox/solvers/smtlib/optimization/Z3Minimizer.scala
+++ src/main/scala/inox/solvers/smtlib/optimization/Z3Minimizer.scala
@@ -11,7 +11,27 @@ trait Z3Minimizer extends Z3Optimizer {
   import program.symbols._
   import exprOps.variablesOf
 
-  private def sizersOf(e: Expr): Seq[Expr] = {
+  /**
+   * Gets the 'zero' literals for the given field,
+   * given the types of "parents" of the expr to avoid infinite recursion.
+   */
+  private def getZeroLiterals(field: ValDef, parentTypes: Set[Type]): Seq[Expr] = field.tpe match {
+    case BVType(signed, size) => Seq(BVLiteral(signed, 0, size))
+    case adt @ ADTType(_, _) =>
+      if (parentTypes.contains(adt)) Seq()
+      else adt.lookupSort match {
+        case Some(tsort) => tsort.constructors.flatMap(_.fields).distinct.flatMap(f => getZeroLiterals(f, parentTypes + adt))
+        case None => Seq()
+      }
+    case IntegerType() => Seq(IntegerLiteral(0))
+    case _ => Seq()
+  }
+
+  /**
+   * Gets the expressions that should be minimized for the given expr,
+   * given the types of "parents" of the expr to avoid an infinite recursion.
+   */
+  private def sizersOf(e: Expr, parentTypes: Set[Type]): Seq[Expr] = {
     /**
      * Gets the expressions that should be minimized for the given ADT constructors.
      * This is not as simple as it first appears because of the split between the bitvector and integer worlds;
@@ -24,17 +44,6 @@ trait Z3Minimizer extends Z3Optimizer {
      *  due to the size in bits vs number of fields; we go with the number of fields)
      */
     def adtSizers(ctors: Seq[TypedADTConstructor]): Seq[Expr] = {
-      /** Gets the 'zero' literals for the given field */
-      def getZeroLiterals(field: ValDef): Seq[Expr] = field.tpe match {
-        case BVType(signed, size) => Seq(BVLiteral(signed, 0, size))
-        case adt @ ADTType(_, _) =>
-          adt.lookupSort match {
-            case Some(tsort) => tsort.constructors.flatMap(_.fields).distinct.flatMap(getZeroLiterals)
-            case None => Seq()
-          }
-        case IntegerType() => Seq(IntegerLiteral(0))
-        case _ => Seq()
-      }
       // zeroes must be of the same length as what sizer returns
       def rec(ctors: Seq[TypedADTConstructor], zeroes: Seq[Expr], sizer: TypedADTConstructor => Seq[Expr]): Seq[Expr] = ctors match {
         case Seq() => zeroes
@@ -43,12 +52,12 @@ trait Z3Minimizer extends Z3Optimizer {
         case Seq(ctor, tl @ _*) => sizer(ctor).zip(rec(tl, zeroes, sizer)).map{case (a,b) => IfExpr(IsConstructor(e, ctor.id), a, b)}
       }
       def fieldSizer(field: ValDef)(ctor: TypedADTConstructor): Seq[Expr] = {
-        if (ctor.fields.contains(field)) sizersOf(ADTSelector(e, field.id)) else getZeroLiterals(field)
+        if (ctor.fields.contains(field)) sizersOf(ADTSelector(e, field.id), parentTypes + field.tpe) else getZeroLiterals(field, parentTypes)
       }
-      // BV32 for the size, because the number of fields can't be larger anyway (since fields.length is an int),
+      // BV32 for the size, because the number of fields can't be larger anyway (since fields.length is a Scala int),
       // let's not force the use of integers if it's not necessary
       val adtSize = rec(ctors, Seq(BVLiteral(false, 0, 32)), c => Seq(BVLiteral(false, c.fields.length, 32)))
-      val fieldSizes = ctors.flatMap(_.fields).distinct.flatMap(f => rec(ctors, getZeroLiterals(f), fieldSizer(f)))
+      val fieldSizes = ctors.flatMap(_.fields).distinct.filter(f => !parentTypes.contains(f.tpe)).flatMap(f => rec(ctors, getZeroLiterals(f, parentTypes), fieldSizer(f)))
       adtSize ++ fieldSizes
     }
 
@@ -56,7 +65,8 @@ trait Z3Minimizer extends Z3Optimizer {
       case BVType(signed, size) => Seq(if (signed) IfExpr(GreaterEquals(e, BVLiteral(signed, 0, size)), e, UMinus(e)) else e)
       case IntegerType() => Seq(IfExpr(GreaterEquals(e, IntegerLiteral(0)), e, UMinus(e)))
       case adt @ ADTType(_, _) =>
-        adt.lookupSort match {
+        if (parentTypes.contains(adt)) Seq()
+        else adt.lookupSort match {
           case Some(tsort) => adtSizers(tsort.constructors)
           case None => Seq()
         }
@@ -65,7 +75,7 @@ trait Z3Minimizer extends Z3Optimizer {
   }
 
   override def assertCnstr(expr: Expr): Unit = {
-    for (freeVar <- variablesOf(expr) ; toMinimize <- sizersOf(freeVar)) {
+    for (freeVar <- variablesOf(expr) ; toMinimize <- sizersOf(freeVar, Set())) {
       minimize(toMinimize)
     }
     super.assertCnstr(expr)
